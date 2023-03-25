77cdd7e9eefd054a8a79acb03473ef6a82aa42aa
Tim <chibuiketimokonkwo@gmail.com>
2023-03-25 13:44:16 +0100

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,29 +18,106 @@ const About = () => {
 	return (
 		<section className="section" id="about">
 			<div className="container mx-auto">
-				<div>
+				<div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
 					{/* Image */}
-					<div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
-
+					<motion.div
+						variants={fadeIn("right", 0.3)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.3 }}
+						className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+					></motion.div>
+    
 					{/* Text */}
-					<div>
-            <h2>About me.</h2>
-            <h3>I'm a Freelance Frontend Developer with over5 years of experience.</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+					<motion.div
+						variants={fadeIn("left", 0.3)}
+						initial="hidden"
+						whileInView={"show"}
+						viewport={{ once: false, amount: 0.3 }}
+						className="flex-1"
+					>
+						<h2 className="h2 text-accent">About me.</h2>
+						<h3 className="h3 mb-4">
+							I'm a Freelance Frontend Developer with over5 years
+							of experience.
+						</h3>
 
-            {/* Stats */}
-            <div className="flex">
-              <div>
-                <div className="text-[40px] font-tertiary text-gradient mb-2">
-                 {inView ? <CountUp start={0} end={13} duration={9} /> : ""}
-                </div>
-                <div className="font-primary text-sm tracking-[2px]">
-                  Years of <br />
-                  Experience.
-                </div>
-              </div>
-            </div>
-          </div>
+						<p className="mb-6">
+							Lorem ipsum, dolor sit amet consectetur adipisicing
+							elit.
+						</p>
+
+						{/* Stats */}
+						<div className="flex gap-x-6 lg:gap-x-12 mb-12">
+							<div>
+								<div
+									ref={ref}
+									className="text-[40px] font-tertiary text-gradient mb-2"
+								>
+									{inView ? (
+										<CountUp
+											start={0}
+											end={13}
+											duration={9}
+										/>
+									) : (
+										""
+									)}
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									Years of <br />
+									Experience.
+								</div>
+							</div>
+							<div>
+								<div
+									ref={ref}
+									className="text-[40px] font-tertiary text-gradient mb-2"
+								>
+									{inView ? (
+										<CountUp
+											start={0}
+											end={13}
+											duration={9}
+										/>
+									) : (
+										""
+									)}
+									k+
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									projects <br /> Completed
+								</div>
+							</div>
+							<div>
+								<div
+									ref={ref}
+									className="text-[40px] font-tertiary text-gradient mb-2"
+								>
+									{inView ? (
+										<CountUp
+											start={0}
+											end={12}
+											duration={9}
+										/>
+									) : (
+										""
+									)}
+									k+
+								</div>
+								<div className="font-primary text-sm tracking-[2px]">
+									Satisfied <br /> Clients
+								</div>
+							</div>
+						</div>
+
+						<div className="flex items-center gap-x-8">
+							<button className="btn btn-lg">Contact me</button>
+							<a href="#" className="text-gradient btn-link">
+								My Porfolio
+							</a>
+						</div>
+					</motion.div>
 				</div>
 			</div>
 		</section>
